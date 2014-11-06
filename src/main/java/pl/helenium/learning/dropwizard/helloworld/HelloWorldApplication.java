4c1e3d69d64f556fcbcd48d59d5edb98cba74e45
Michał Mally <michal@mally.pl>
2014-11-06 10:14:30 +0100

    First working version of Dropwizard Hello World.

---
+++ src/main/java/pl/helenium/learning/dropwizard/helloworld/HelloWorldApplication.java
@@ -0,0 +1,40 @@
+package pl.helenium.learning.dropwizard.helloworld;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import pl.helenium.learning.dropwizard.helloworld.health.TemplateHealthCheck;
+import pl.helenium.learning.dropwizard.helloworld.resources.HelloWorldResource;
+
+public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+
+    public static void main(String[] args) throws Exception {
+        new HelloWorldApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "hello-world";
+    }
+
+    @Override
+    public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+        // nothing to do yet
+    }
+
+    @Override
+    public void run(HelloWorldConfiguration configuration,
+                    Environment environment) {
+        final HelloWorldResource resource = new HelloWorldResource(
+                configuration.getTemplate(),
+                configuration.getDefaultName()
+        );
+        environment.jersey().register(resource);
+
+        final TemplateHealthCheck healthCheck =
+                new TemplateHealthCheck(configuration.getTemplate());
+        environment.healthChecks().register("template", healthCheck);
+        environment.jersey().register(resource);
+    }
+
+}
