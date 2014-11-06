964eb7afbd3af005973baf21719aaa223ecae5a5
Michał Mally <michal@mally.pl>
2014-11-06 19:32:41 +0100

    Awful writting.

--- src/main/java/pl/helenium/learning/dropwizard/helloworld/resources/FeedConsumer.java
+++ src/main/java/pl/helenium/learning/dropwizard/helloworld/resources/FeedConsumer.java
@@ -2,13 +2,15 @@ package pl.helenium.learning.dropwizard.helloworld.resources;
 
 import org.apache.commons.io.IOUtils;
 
-import javax.activation.MimeType;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.zip.GZIPInputStream;
+import java.io.OutputStream;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+
+import static java.nio.file.StandardOpenOption.APPEND;
 
 @Path("/")
 @Consumes("text/plain")
@@ -17,13 +19,15 @@ public class FeedConsumer {
     @POST
     @Path("/feed-consumer")
     public void consume(String body) throws IOException {
-        System.out.println(body);
+        OutputStream outputStream = Files.newOutputStream(FileSystems.getDefault().getPath(".", "feed-consumer"), APPEND);
+        IOUtils.write(body, outputStream);
     }
 
     @POST
     @Path("/verify")
-    public void verify(String body) {
-        System.out.println(body);
+    public void verify(String body) throws IOException {
+        OutputStream outputStream = Files.newOutputStream(FileSystems.getDefault().getPath(".", "verify"), APPEND);
+        IOUtils.write(body, outputStream);
     }
 
 }
